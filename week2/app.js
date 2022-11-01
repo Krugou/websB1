564c47056230c7b6aa5d60d9f9ec5ac8651d52cb
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-01 11:23:42 +0200

    Update app.js

--- week2/app.js
+++ week2/app.js
@@ -1,21 +1,10 @@
 'use strict';
 const express = require('express');
+const catroute = require('./routes/catRoute');
 const app = express();
 const port = 3000;
 
-app.get('/cat', (req, res) => {
-  res.send('From this endpoint you can get cats.');
-});
-app.post('/cat', (req, res) => {
-  res.send('From this endpoint you can add cats.');
-});
-app.put('/cat', (req, res) => {
-  res.send('From this endpoint you can edit cats.');
-});
-app.delete('/cat', (req, res) => {
-  res.send('From this endpoint you can remove cats.');
-});
-app.get('/cat/:id', (req, res) => { ;
-  res.send('you requested a cat whose id is ' + req.params.id);
-});
+
+app.use('/cat', catroute);
+
 app.listen(port, () => console.log(`käynnistyi portilla ${port}!`));
