0e9c7937d29352a34b201e210d9cf2ead9fa07fd
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-01 11:23:36 +0200

    Update catRoute.js

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,2 +1,21 @@
 'use strict';
-// catRoute
+
+const express = require('express');
+const router = express.Router();
+const {cat_list_get} = require('../controllers/catController');
+router.get('/', cat_list_get);
+router.post('/', (req, res) => {
+  res.send('From this endpoint you can add cats.');
+});
+router.put('/', (req, res) => {
+  res.send('From this endpoint you can edit cats.');
+});
+router.delete('/', (req, res) => {
+  res.send('From this endpoint you can remove cats.');
+});
+router.get('/:id', (req, res) => {
+  res.send('you requested a cat whose id is ' + req.params.id);
+});
+
+
+module.exports = router;
