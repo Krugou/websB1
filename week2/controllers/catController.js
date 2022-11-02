2795ae0c08d1799433b17581ca220be88a2d060f
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-02 15:14:56 +0200

    Update catController.js

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -7,6 +7,11 @@ const cat_get = (req,res) => {
     const cat = getCat(req.params.id)
     console.log('kissa ', cat);
     res.json(cat);
+};
+  const cat_post = (req, res) => {
+    console.log('cat_post', req.body);
+    console.log('tiedosto ', req.file);
+    res.send('cat post done');
   };
 
-module.exports = {cat_list_get, cat_get,};
+module.exports = {cat_list_get, cat_get, cat_post};
