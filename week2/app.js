83e6688eef24cf4e049ced2199a0b9f37ff465f5
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-06 19:50:35 +0200

    asd

--- week2/app.js
+++ week2/app.js
@@ -1,10 +1,17 @@
 'use strict';
 const express = require('express');
-const catroute = require('./routes/catRoute');
+const cors = require('cors');
+const catRoute = require('./routes/catRoute');
+const userRoute = require('./routes/userRoute');
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('uploads'));
 
-app.use('/cat', catroute);
+app.use('/cat', catRoute);
+app.use('/user', userRoute);
 
 app.listen(port, () => console.log(`käynnistyi portilla ${port}!`));
