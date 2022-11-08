14cfdf308e480811814acda9d484982afd713488
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-08 11:00:57 +0200

    Update app.js

--- week2/app.js
+++ week2/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
+const { httpError } = require('./utils/errors');
 const app = express();
 const port = 3000;
 
@@ -14,4 +15,18 @@ app.use(express.static('uploads'));
 app.use('/cat', catRoute);
 app.use('/user', userRoute);
 
-app.listen(port, () => console.log(`käynnistyi portilla ${port}!`));
+app.use((req, res, next) => {
+  const err = httpError('Not found', 404);
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || 'Internal server error' });
+});
+
+app.listen(port, () => console.log(`käynnistyi portilla ${port}!`
+));
+
+
