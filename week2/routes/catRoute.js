89ae64229cacc06b0926ec88704fb52c7d60445e
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-10 11:29:48 +0200

    Update catRoute.js

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const multer = require('multer');
+const { body } = require('express-validator');
 const upload = multer({ dest: 'uploads/' });
 const {
   cat_list_get,
@@ -14,8 +15,22 @@ const router = express.Router();
 router
   .route('/')
   .get(cat_list_get)
-  .post(upload.single('cat'), cat_post)
-  .put(cat_put);
+  .post(
+    upload.single('cat'),
+    body('name').isLength({ min: 1 }).escape(),
+    body('birthdate').isDate(),
+    body('weight').isNumeric(),
+    body('owner').isNumeric(),
+    body('id').isNumeric(),
+    cat_post
+  )
+  .put(
+    body('name').isLength({ min: 1 }).escape(),
+    body('birthdate').isDate(),
+    body('weight').isNumeric(),
+    body('owner').isNumeric(),
+    cat_put
+  );
 
 router.route('/:id').get(cat_get).delete(cat_delete);
 
