2ee6a979fdfba8ba719614eac6c238183cc92fb1
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-17 10:04:20 +0200

    Update userroute.js

--- week2/routes/userroute.js
+++ week2/routes/userroute.js
@@ -6,8 +6,10 @@ const {
   user_post,
   user_put,
   user_delete,
+  check_Token,
 } = require('../controllers/userController');
 const { body } = require('express-validator');
+
 
 const router = express.Router();
 
@@ -21,7 +23,8 @@ router
     user_post
   )
   .put(user_put);
-router.get('/token', check_token);
+
 router.route('/:id').get(user_get).delete(user_delete);
+router.get('/token', check_Token);
 
 module.exports = router;
