dbdfc2f747be870ceac1ba1416ce499bd7641cd3
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-22 10:40:20 +0200

    Update authRoute.js

--- week2/routes/authRoute.js
+++ week2/routes/authRoute.js
@@ -2,7 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const { login } = require('../controllers/authController');
+const { user_post } = require('../controllers/userController');
 
 router.post('/login', login);
+
+router.post('/register',
+    body('name').isLength({ min: 3 }).escape(),
+    body('email').isEmail(),
+    body('passwd').matches(/(?=.*[\p{Lu}]).{8,}/u), user_post);
 
 module.exports = router;
