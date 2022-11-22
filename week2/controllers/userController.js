541da134b73bdb3d8fa20c1cb9bafdc1140ca43a
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-22 12:14:33 +0200

    Update userController.js

--- week2/controllers/userController.js
+++ week2/controllers/userController.js
@@ -1,12 +1,6 @@
 'use strict';
 // userController
-const {
-  getUser,
-  getAllUsers,
-  addUser,
-  deleteUser,
-  updateUser,
-} = require('../models/userModel');
+const { getUser, getAllUsers, deleteUser, updateUser } = require('../models/userModel');
 const { validationResult } = require('express-validator');
 const { httpError } = require('../utils/errors');
 
@@ -38,36 +32,7 @@ const user_get = async (req, res, next) => {
   }
 };
 
-const user_post = async (req, res, next) => {
-  try {
-    // Extract the validation errors from a request.
-    const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      // There are errors.
-      // Error messages can be returned in an array using `errors.array()`.
-      console.error('user_post validation', errors.array());
-      next(httpError('Invalid data', 400));
-      return;
-    }
-
-    const data = [req.body.name, req.body.email, req.body.passwd];
-
-    const result = await addUser(data, next);
-    if (result.affectedRows < 1) {
-      next(httpError('Invalid data', 400));
-      return;
-    }
-
-    res.json({
-      message: 'user added',
-      user_id: result.insertId,
-    });
-  } catch (e) {
-    console.error('user_post', e.message);
-    next(httpError('Internal server error', 500));
-  }
-};
 
 const user_put = async (req, res, next) => {
   try {
@@ -114,7 +79,7 @@ const check_Token = (req, res, next) => {
 module.exports = {
   user_list_get,
   user_get,
-  user_post,
+  
   user_put,
   user_delete,
   check_Token,
