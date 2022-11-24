137f4d94354d0045d3647fb7232211ed49608890
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-24 09:17:38 +0200

    Update catRoute.js

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,39 +1,37 @@
 'use strict';
 const express = require('express');
-const multer = require('multer');
 const { body } = require('express-validator');
-const upload = multer({ dest: 'uploads/' });
-const {
-  cat_list_get,
-  cat_get,
-  cat_post,
-  cat_put,
-  cat_delete,
-} = require('../controllers/catController');
+const { httpError } = require('../utils/errors');
+const multer = require('multer');
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.includes('image')) {
+    cb(null, true);
+  } else {
+    cb(httpError('Invalid file', 400));
+  }
+}
+
+const upload = multer({ dest: 'uploads/', fileFilter });
+const { cat_list_get, cat_get, cat_post, cat_put, cat_delete } = require(
+  '../controllers/catController');
 const router = express.Router();
 
-router
-  .route('/')
-  .get(cat_list_get)
-  .post(
-    upload.single('cat'),
+
+router.route('/').
+  get(cat_list_get).
+  post(upload.single('cat'),
     body('name').isLength({ min: 1 }).escape(),
     body('birthdate').isDate(),
     body('weight').isNumeric(),
+    cat_post);
 
-    cat_post
-  );
-
-router
-  .route('/:id') 
-  .get(cat_get) // get cat by id
-  .delete(cat_delete) // delete cat by id
-  .put( // update cat by id
-    body('name').isLength({ min: 1 }).escape(),
+router.route('/:id').
+  get(cat_get).
+  delete(cat_delete).
+  put(body('name').isLength({ min: 1 }).escape(),
     body('birthdate').isDate(),
     body('weight').isNumeric(),
-    body('id').isNumeric(),
-    cat_put
-  );
+    cat_put);
 
 module.exports = router;
