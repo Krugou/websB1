a3c799daac730b5d5fa73d6900c9dd23e930c204
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-27 03:22:55 +0200

    Update localhost.js

--- week2/utils/localhost.js
+++ week2/utils/localhost.js
@@ -2,21 +2,5 @@
 
 module.exports = (app, port) => {
     app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-    console.log('              __ ');
-    console.log('             .¨  ¨. ');
-    console.log('             :      :');
-    console.log('             | _  _ |');
-    console.log('          .-.|(o)(o)|.-.        _._          _._');
-    console.log('         ( ( | .--. | ) )     .¨,_ ¨.      .¨ _,¨.');
-    console.log('          ¨-/ (    ) -¨     / /¨ `  __ / /¨ `  ');
-    console.log('           /   ¨--¨        / /     .¨  ¨./       ');
-    console.log('            `"===="` /     `-`     : _  _ :      `-`');
-    console.log('            `      /¨              |(o)(o)|');
-    console.log('              `  /¨                |      |');
-    console.log('              /`-.-`_             /         ');
-    console.log('        _..:;._/V_./:;.._       /   .--.    ');
-    console.log('      .¨/;:;:; /^ /:;:;:¨.     |  (    )  | ');
-    console.log('     / /;:;:;:;| |/:;:;:;:     _  ¨--¨  /__');
-    console.log('    / /;:;:;:;:;_/:;:;:;:;:  .¨  ¨-.__.-¨   `-.');
 
 }
