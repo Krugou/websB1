b826e481ddabfc25fd924313c65dbc375488cc08
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-27 02:17:59 +0200

    Update errors.js

--- week2/utils/errors.js
+++ week2/utils/errors.js
@@ -4,5 +4,9 @@ const httpError = (message, status) => {
     const err = new Error(message);
     err.status = status;
     return err;
+};
+
+
+module.exports = {
+    httpError,
 }
-module.exports = {httpError}
